b2b7a77dd2106f4e6e0f3a8171f75d4dfc5a781a
monospacedmagic <luci@monospacedmagic.dev>
2022-07-27 16:35:26 +0200

    Redo the 'rush create' command

--- tools/generator/generate.sh
+++ tools/generator/generate.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-exec "rush-pnpm" "--rush-skip-checks" "hygen" "$@";
+if ! [ -d "$(dirname $(readlink -f $0))/node_modules" ]
+then
+    echo "Error: not installed. Make sure to run 'rush install' before using 'rush create'."
+    exit 1
+fi
+
+project_type=$1
+project_name=$2
+
+target_path=$RUSH_INVOKED_FOLDER
+
+case $project_type in
+    app)
+        target_path=$MONOREPO_ROOT/apps;;
+    component)
+        target_path=$MONOREPO_ROOT/components;;
+    extension)
+        target_path=$MONOREPO_ROOT/extensions;;
+    library)
+        target_path=$MONOREPO_ROOT/libraries;;
+    service)
+        target_path=$MONOREPO_ROOT/services;;
+    tool)
+        target_path=$MONOREPO_ROOT/tools;;
+    template)
+        target_path=$MONOREPO_ROOT/templates;;
+    meta)
+        target_path=$MONOREPO_ROOT/meta;;
+    *)
+        echo "Error: unrecognized project type '${project_type}'. See ${MONOREPO_ROOT}/templates/README.md for available options.";
+        exit 1;;
+esac
+
+cd $target_path
+
+"${MONOREPO_ROOT}/$(dirname $0)/node_modules/.bin/hygen" "$project_type" "new" "$project_name"
